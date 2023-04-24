1b8bd031b923d88cfc5c82bc0202bc3fc45209a9
Ирина <papahinairina@gmail.com>
2023-04-24 19:45:31 +0300

    made wrapper, container

---
+++ my-portfolio/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+
+
+export default function Header() {
+
+  return(
+    <div className="wrapper">
+      <div className="container">
+        <div className="info"> About me </div>
+
+        <div> Contact</div>
+      </div>
+    </div>
+  )
+}
