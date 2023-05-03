a03778a56102e331d69146b49a9286638fe47d87
Ирина <papahinairina@gmail.com>
2023-05-03 20:04:25 +0300

    finished Case2-Widget

--- my-portfolio/src/components/Portfolio/Cases/Widget/Widget.js
+++ my-portfolio/src/components/Portfolio/Cases/Widget/Widget.js
@@ -2,14 +2,7 @@ import React, {useState} from "react";
 import { Icon } from '@iconify/react';
 
 
-import './style.scss';
-
-
-const tabWidth = 300;
-
-
-
-
+import './style.css';
 
 export default function Widget () {
 
@@ -18,56 +11,44 @@ export default function Widget () {
   return (
     <article className="widget">
       <header className="containerIcon">
-        <div>
+        <div className={activeIndex === 1 && "underline"}>
           <button id="1" onClick={() => setActiveIndex(1)}>
             <Icon icon="carbon:home"width={56} />
           </button>
         </div>
 
-        <div>
+        <div className={activeIndex === 2 && "underline"}>
           <button id="2" onClick={() => setActiveIndex(2)}>
             <Icon icon="ic:round-lock-open"width={56} />
           </button>
         </div>
 
-        <div>
+        <div className={activeIndex === 3 && "underline"}>
           <button id="3" onClick={() => setActiveIndex(3)}>
             <Icon icon="ic:outline-settings"width={56} />
           </button>
         </div>
-
-{/* 
-        {tabHeaders.map((tab, index) => (
-          <button
-          onClick={() => setActiveIndex(index)}
-          key={tab}
-          class={`material-symbol-outlined ${activeIndex === index ? "active" : ""}`}
-          >
-            {tab}
-          </button>
-        ))}
-        <div className="underline"
-        style={{
-          translate: `${activeIndex ? activeIndex * buttonWidth : 0}px 0`
-        }}>
-        </div> */}
       </header>
 
       <div className="content">
         <div className="content-inner"
-        style={{
-          translate: `-${activeIndex ? activeIndex * tabWidth : 0} px 0`
-        }}
         >
+          {activeIndex === 1 && (
           <div>
+            Home
           </div>
+          )}
           
-
+          {activeIndex === 2 && (
           <div>
+            Lock
           </div>
-
+          )}
+          {activeIndex === 3 && (
           <div>
+            Settings
           </div>
+          )}
 
         </div>
       </div>
