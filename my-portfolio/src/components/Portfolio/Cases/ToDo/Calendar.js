6033de05859bcacd8be893597db162ff7a48c0da
Ирина <papahinairina@gmail.com>
2023-05-16 23:06:40 +0300

    created db.json

--- my-portfolio/src/components/Portfolio/Cases/ToDo/Calendar.js
+++ my-portfolio/src/components/Portfolio/Cases/ToDo/Calendar.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import moment from 'moment';
 import './style.scss';
 
-
+const url = 'http://localhost:5000';
+const totalDays = 42;
 
 export default function Calendar () {
-
-  const totalDays = 42;
   const startDate =  moment().startOf('week');
   const endDate = moment().endOf('week');
+
+  const startDateQuery = startDate.clone().format('X');
+  const endDateQuery = endDate.clone().add(totalDays, 'days').format('X');
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch(`${url}/events?date_gte=${startDateQuery}&date_lte=${endDateQuery}`)
+    .then(res => res.json)
+    .then(res => setEvents(res))
+  }, []);
+
 
   const day = startDate.clone();
   const arrDays = [...Array(totalDays)].map(() => day.add(1, 'day').clone());
@@ -22,23 +34,23 @@ export default function Calendar () {
           <div className='weekWrapper'> 
             {moment().day(i + 1).format('ddd')}
           </div>
-      ))}
-      {
-        arrDays.map((dayItem) => ( 
-          <div className={isCurrentMonth(dayItem) ? 'currentMonth' : 'dayWrapper'} 
-            key={dayItem.unix()}
-            isWeekend={dayItem.day === 6 || dayItem.day === 0}
-            isCurrentMonth={isCurrentMonth(dayItem)}
-            >
-            <div className='rowInCell'>
-              <div className={ isCurrentDay(dayItem) ? 'currentDay' : '' }>
-                {dayItem.format('D')}
+        ))}
+        {
+          arrDays.map((dayItem) => ( 
+            <div className={isCurrentMonth(dayItem) ? 'currentMonth' : 'dayWrapper'} 
+              key={dayItem.unix()}
+              isWeekend={dayItem.day === 6 || dayItem.day === 0}
+              isCurrentMonth={isCurrentMonth(dayItem)}
+              >
+              <div className='rowInCell'>
+                <div className={ isCurrentDay(dayItem) ? 'currentDay' : '' }>
+                  {dayItem.format('D')}
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      }
-    </div>
+          ))
+        }
+      </div>
     </>
   )
 } 
